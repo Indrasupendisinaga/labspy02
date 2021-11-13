a1ae83e122f2438025f78e13fef73776d9351c56
indrasupendisinaga <indrasupendisinaga@gmail.com>
2021-11-13 09:38:48 +0700

    penambahan gambar

--- Latihan2.py
+++ Latihan2.py
@@ -2,3 +2,27 @@ print ("------------------")
 print ("Pertemuan ke 7")
 print ("latihan 2")
 print ("------------------")
+
+gaji = int(input("masukkan gaji :"))
+berkeluarga = (False, True)[input("sudah berkeluarga? (Y/T) :") == "Y"]
+punya_rumah = (False, True)[input("punya rumah? (Y/T) :") == "Y"]
+
+if gaji > 3000000:
+
+
+    print ("                        ")
+    print ("-------Keterangan-------")
+    print ("                        ")
+
+    print ("gaji sudah diatas UMR")
+    if berkeluarga :
+        print ("- wajib ikutan asuransi dan menabung untuk pensiun")
+    else:
+        print ("- tidak perlu ikut asuransi")    
+    if punya_rumah :
+        print ("- wajib bayar pajak rumah")
+    else:
+        print ("- tidak wajib bayar pajak rumah")
+else:
+    print ("gaji belum UMR")
+    
--- Latihan3.py
+++ Latihan3.py
@@ -1,4 +1,17 @@
 print ("------------------")
 print ("Pertemuan ke 7")
 print ("latihan 3")
+print ("penggunaan kondisi or, program membandingkan 3 input bilangan")
 print ("------------------")
+
+
+
+a = int (input("masukkan bilangan A :"))
+b = int (input("masukkan bilangan B :"))
+c = int (input("masukkan bilangan C :"))
+
+print ("------------------")
+if a+b == c or b+c == a or c+a == b :
+    print("BENAR")
+else :
+    print("SALAH")
