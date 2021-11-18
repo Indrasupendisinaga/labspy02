d56552036e9c9dba8a3397f16fc636fee668e128
indrasupendisinaga <indrasupendisinaga@gmail.com>
2021-11-18 16:02:01 +0700

    penambahan gambar tugas praktikum

--- Tugaspraktikum2.py
+++ Tugaspraktikum2.py
@@ -2,3 +2,16 @@ print ("------------------")
 print ("Pertemuan ke 7")
 print ("Tugas praktikum 2")
 print ("------------------")
+
+a, b, c = input ()
+if a>b:
+    if b > c:
+        maks = a
+    else :
+        if c > a:
+            maks : c
+        elif a > c:
+                maks = b 
+        elif c > b:
+                    maks = c
+    print maks
